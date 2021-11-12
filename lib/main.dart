c53932b6e7a49bacf5924d205bc8bc5c757a13ea
Luis Flores <lf016158@gmail.com>
2021-11-11 18:04:34 -0600

    se agrego una caja de texto,titulo y un mensaje de dialogo

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,32 @@ class Myapp extends StatelessWidget
 
    Widget build(BuildContext context) {
      return MaterialApp(
+       title: "Ejercicio de Insercion de texto",
        home: HomePage(),
      );
    }
 }
 
 //Creacion de Clase y imprimimos hola mundo en pantalla
+//Creacion de widget de pagian de inicio
 class HomePage extends StatelessWidget {
+    final myField = TextEditingController();
     @override 
 
    Widget build(BuildContext context) {
      return Scaffold(
-       appBar: AppBar(),
-       floatingActionButton: FloatingActionButton(onPressed: (){},
+       appBar: AppBar(
+         title: const Text("Mi App"),
+       ),
+       body: Padding(padding: const EdgeInsets.all(16.0),child: TextField(controller: myField,)),
+       floatingActionButton: FloatingActionButton(onPressed: (){
+         showDialog(context: context, builder: (context)
+         {
+           return AlertDialog(
+             content: Text(myField.text),
+           );
+         });
+       },
        child: Icon(Icons.add,)),
      );
    }
